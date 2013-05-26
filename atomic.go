242f875c1f5782c1d81a92d547ac57a9cf0ddbff
frio <development@frio.name>
2013-05-26 18:49:03 +1200

    Adds a (very rough!) API for LimitlessLED bulbs
    This is pretty unpolished.  I wanted to get it committed so that I can
    stick to my "every Sunday I must get something in" goal -- otherwise,
    I'd probably never get the drive to finish it.  Hopefully now the shame
    of an ugly commit will lead me to refactoring it!
    
    In particular, the database handling is rough, as is the type
    conversions.  Also, now I have 3 config files -- that seems silly (all
    the config for these should be in a database anyway!).

--- atomic.go
+++ atomic.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	_ "code.google.com/p/go-sqlite/go1/sqlite3" // provides sqlite3 driver
 	"code.google.com/p/gompd/mpd"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/frio/limitlessled"
 	"github.com/ghthor/gowol"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
-var computers map[string]string
-var alarms map[string]string
+var (
+	computers map[string]string
+	alarms    map[string]string
+	rooms     map[string]string
+)
 
 func AlarmHandler(w http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
@@ -51,20 +58,65 @@ func ComputerHandler(w http.ResponseWriter, request *http.Request) {
 	} else {
 		http.NotFound(w, request)
 	}
+}
 
+func RoomHandler(w http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	isOn := strings.ToLower(request.FormValue("isOn")) == "true"
+	brightness64, err := strconv.ParseInt(request.FormValue("brightness"), 10, 8)
+	temperature64, err := strconv.ParseInt(request.FormValue("temperature"), 10, 8)
+
+	if err != nil {
+		http.Error(w, "Couldnt read values", 400)
+		return
+	}
+
+	brightness := int(brightness64)
+	temperature := int(temperature64)
+
+	if bridgeAddress, ok := rooms[params["name"]]; ok {
+		// initialize DB conn
+		var states *limitlessled.States
+		conn, err := sql.Open("sqlite3", "./state.db")
+
+		if err != nil {
+			http.Error(w, "No stateful DB", 500)
+			return
+		}
+
+		// defer conn.Close()
+		states = &limitlessled.States{conn}
+		originalState, err := states.Retrieve()
+
+		if err != nil {
+			http.Error(w, "Couldn't get current state", 500)
+		}
+
+		newState := limitlessled.Bulb{Brightness: brightness, Temperature: temperature, IsOn: isOn}
+
+		bridge, err := limitlessled.Dial(bridgeAddress)
+		currentState := bridge.Set(*originalState, newState)
+
+		states.Store(currentState)
+	} else {
+		http.NotFound(w, request)
+	}
 }
 
 func main() {
 	compConfig, compErr := ioutil.ReadFile("computers.json")
 	alarmConfig, alarmErr := ioutil.ReadFile("alarms.json")
+	roomConfig, roomErr := ioutil.ReadFile("rooms.json")
 
-	if compErr == nil && alarmErr == nil {
+	if compErr == nil && alarmErr == nil && roomErr == nil {
 		json.Unmarshal(compConfig, &computers)
 		json.Unmarshal(alarmConfig, &alarms)
+		json.Unmarshal(roomConfig, &rooms)
 
 		r := mux.NewRouter()
 		r.HandleFunc("/computer/{name}/", ComputerHandler)
 		r.HandleFunc("/alarm/{name}/", AlarmHandler)
+		r.HandleFunc("/room/{name}/", RoomHandler)
 		http.Handle("/", r)
 		http.ListenAndServe("localhost:5000", nil)
 	} else {
